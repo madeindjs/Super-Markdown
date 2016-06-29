9564266dfb89bc927489615c55dd2ccc51e2976e
madeindjs <madeindjs@gmail.com>
2016-06-29 09:53:51 +0200

    use BeatifullSoup library

--- mardown-exporter.py
+++ mardown-exporter.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import argparse
 import os
 import sys
 import webbrowser
-# I haven't internet conection to install now :(
+
 import markdown
 from markdown.extensions.toc import TocExtension
-
 from graphviz import Digraph
+from bs4 import BeautifulSoup
 
 
 
 if __name__ == '__main__':
+
+
 
 	def get_text_file(url):
 		"""return the content of all files"""
@@ -21,6 +26,8 @@ if __name__ == '__main__':
 			print('File `{}` not found'.format(url))
 			sys.exit(0)
 
+
+
 	def get_ressources(directory):
 		"""return all files content in a directory in a big big string"""
 		ret = str()
@@ -28,6 +35,7 @@ if __name__ == '__main__':
 			ret += get_text_file('{}/{}'.format(directory, file))
 
 		return ret
+
 
 
 	def search_replace_graphiz():
@@ -51,48 +59,47 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 
-
+	# create the main soup from the `snippert.html` file
 	text = get_text_file('ressources/snippet.html')
+	soup = BeautifulSoup(text.encode('utf-8'), 'html.parser')
+
+	# insert css files and js files
+	soup.script.append( get_ressources('ressources/js') )
+	soup.style.append( get_ressources('ressources/css') )
 
 	# insert all files ressources in text
 	graph = search_replace_graphiz()
-	print(graph)
 	text = text.replace('<<graphiz_here>>',graph )
 
-	text = text.replace('<<styles_here>>', get_ressources('ressources/css'))
-	text = text.replace('<<scripts_here>>', get_ressources('ressources/js'))
-
-
-
+	# Get the markdown text from file (parse arg to find file(s))
 	markdown_text = str()
 
-	if args.directory:
+	if args.directory:# get all files from directory
 		markdown_text = "\r\n[TOC]\r\n"
 		files=[file for file in os.listdir(args.directory) if not os.path.isdir(file)]
-
 		for file in files:
 			markdown_text+=get_text_file('{}\{}'.format(args.directory, file))
 			markdown_text += "{rn}{sep}{rn}".format(rn="\r\n", sep="---------")
 
-	elif args.file: 
+	elif args.file:# get the file from directory
 		markdown_text = get_text_file( args.file )
 
-	else:
+	else:# get the default markdown file `ressources/test.markdown`
 		markdown_text = get_text_file( 'ressources/test.markdown' )
 
 
 	markdown_html = markdown.markdown(markdown_text, 
 		extensions=[
-			TocExtension(), 
-			'fenced_code', 
-			'markdown_checklist.extension'] )
-	text = text.replace('<<content>>', markdown_html)
+			TocExtension(), 'fenced_code', 'markdown_checklist.extension'] )
+
+
+	soup.body.append( BeautifulSoup(markdown_html, 'html.parser') )
 
 	export_url = 'export.html'
 
 	try:
 		file = open(export_url,'w', encoding='utf-8')
-		file.write(text)
+		file.write(soup.prettify())
 		webbrowser.open_new_tab(export_url)
 	except FileNotFoundError :
 		print('Can\'t open/create `export.html` file (maybe already opened?)')
